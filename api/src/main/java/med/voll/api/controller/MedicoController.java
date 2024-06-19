ff0c8857b6d622d9d139d1d1968b04879bcca9f1
Sallenkon S. Almeida <128660433+SallenSan@users.noreply.github.com>
2024-06-19 13:19:29 -0300

    paginacao
    Foi alterada a requisição GET da medico controller, alterando de List para Page

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,8 +30,8 @@ public class MedicoController {
     // se colocar return list de medicos ele trara varias informacoes como endereco, telefone, e sao informcaoes confidenciais
     // usaremos uma DTO chamada DadosListagemMedico onde convertera o medico para listagem, contendo apenas os dados necessarios
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
 
     }
 }
